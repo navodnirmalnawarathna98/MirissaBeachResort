e1eaeefa80279e8f19d9a7005e44a1c27a827a9f
chamodi pathirana IT20621124 <96380182+cc-cham@users.noreply.github.com>
2022-05-08 00:22:49 +0530

    Update EventBooking.java
    create event dashboard

--- app/src/main/java/com/example/mirissabeachresort/EventBooking.java
+++ app/src/main/java/com/example/mirissabeachresort/EventBooking.java
@@ -2,7 +2,9 @@ package com.example.mirissabeachresort;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EventBooking extends AppCompatActivity {
 
@@ -11,4 +13,18 @@ public class EventBooking extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_booking);
     }
+
+    public void gregoryBallroom(View view) {
+        startActivity(new Intent(getApplicationContext(),EventBooking_GregoryBallroom_Dis1.class));
+    }
+    public void grandBallroom(View view) {
+        startActivity(new Intent(getApplicationContext(),EventBooking_GrandBallroom_Dis1.class));
+    }
+    public void beach(View view) {
+        startActivity(new Intent(getApplicationContext(),EventBooking_Beach_Dis1.class));
+    }
+    public void dining(View view) {
+        startActivity(new Intent(getApplicationContext(),EventBooking_Dining_Dis1.class));
+    }
+
 }
