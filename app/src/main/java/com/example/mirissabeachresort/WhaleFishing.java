2ff0dcaa082cd760de7fe9ff7fead60d2ca70566
Navod Nirmal Nawarathna <novanawarathna@gmail.com>
2022-05-07 11:56:06 +0530

    Update WhaleFishing.java
    navigate to Whale Watching Details

--- app/src/main/java/com/example/mirissabeachresort/WhaleFishing.java
+++ app/src/main/java/com/example/mirissabeachresort/WhaleFishing.java
@@ -2,7 +2,9 @@ package com.example.mirissabeachresort;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WhaleFishing extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class WhaleFishing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whale_fishing);
+    }
+
+    public void whaleDash(View view) {
+        startActivity(new Intent(getApplicationContext(),WhaleWatching_Details.class));
     }
 }
