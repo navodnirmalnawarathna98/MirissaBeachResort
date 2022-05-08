32dddd18287ffaee2ef5742827244de79e785ed4
Dilshan Sudharaka <94373697+dilshan-sudharaka@users.noreply.github.com>
2022-05-08 08:12:22 +0530

    crate customer buffet dis 2 page

--- app/src/main/java/com/example/mirissabeachresort/FoodBuffet_Buffet_Dis2.java
+++ app/src/main/java/com/example/mirissabeachresort/FoodBuffet_Buffet_Dis2.java
@@ -2,7 +2,9 @@ package com.example.mirissabeachresort;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FoodBuffet_Buffet_Dis2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class FoodBuffet_Buffet_Dis2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_buffet_buffet_dis2);
+    }
+
+    public void buffetBooking(View view) {
+        startActivity(new Intent(getApplicationContext(),FoodBuffet_Booking_Form.class));
+    }
+
+    public void profile(View view) {
+        startActivity(new Intent(getApplicationContext(),FoodBuffet_Buffet_Dis1.class));
     }
 }
