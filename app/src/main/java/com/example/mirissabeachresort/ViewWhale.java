febee0e938649607f030934d40ea346094a41fdf
Navod Nirmal Nawarathna <novanawarathna@gmail.com>
2022-05-10 12:38:47 +0530

    update
    update ui and details

---
+++ app/src/main/java/com/example/mirissabeachresort/ViewWhale.java
@@ -0,0 +1,14 @@
+package com.example.mirissabeachresort;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewWhale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_whale);
+    }
+}
